bde5153b4a9a797ce4c9d562ca8b9b4af8ce3e78
Farook Kuruvanna <getfarook@gmail.com>
2022-11-15 12:09:53 +0530

    new directories

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qktrade_app/screens/reports/reports_tab_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,30 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Text('Hello World'),
-      ),
+      home: ReportsTabScreen(),
     );
   }
 }
